8c5f70d529aaf0b20daa44564ac7c561dbb44c82
Justin Moore <JustinTMoore@comcast.net>
2019-08-24 22:12:22 -0400

    Added Cards That Dynamically Resize

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
+  contest = {
+    id: 'testId',
+    createDateTime: '2019-01-01T12:00:00:000Z',
+    closeDateTime: '2019-01-01T13:00:00:000Z',
+    description: 'going out',
+    style: 'casual',
+    options: [
+      {
+        id: 1,
+        imageUrl: 'https://via.placeholder.com/1080x1920?text=Image_1',
+        votes: 2
+      },
+      {
+        id: 2,
+        imageUrl: 'https://via.placeholder.com/1080x1920?text=Image_2',
+        votes: 6
+      }
+    ]
+  };
   constructor() {}
-
 }
