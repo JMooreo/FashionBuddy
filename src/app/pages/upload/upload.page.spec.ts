58f99e2c465dae0bd4de6a1bbb9c874e1ee86f94
Justin Moore <JustinTMoore@comcast.net>
2019-09-08 19:16:34 -0400

    Satisfy ng-test

--- src/app/pages/upload/upload.page.spec.ts
+++ src/app/pages/upload/upload.page.spec.ts
@@ -2,9 +2,9 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { UploadPage } from './upload.page';
-import { AuthService } from 'src/app/services/auth/auth.service';
 import { DatabaseService } from 'src/app/services/database/database.service';
 import { ContestOption, Contest } from 'src/app/models/contest-model';
+import { SharedComponentsModule } from 'src/app/components/components.module';
 
 describe('UploadPage', () => {
   let component: UploadPage;
@@ -13,7 +13,7 @@ describe('UploadPage', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [UploadPage],
-      imports: [IonicModule.forRoot()],
+      imports: [IonicModule.forRoot(), SharedComponentsModule],
       providers: [
         // tslint:disable-next-line: no-use-before-declare
         { provide: DatabaseService, useClass: DatabaseServiceStub }
