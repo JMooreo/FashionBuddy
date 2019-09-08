e02e436160d6916029e97e097db480a8003c84f5
Justin Moore <JustinTMoore@comcast.net>
2019-09-08 11:54:45 -0400

    Better Registration and Login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,26 +1,51 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { NavController, AlertController } from '@ionic/angular';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  styleUrls: ['./login.page.scss']
 })
 export class LoginPage implements OnInit {
+  email = '';
+  password = '';
 
-  constructor(private navCtrl: NavController) { }
+  constructor(
+    private navCtrl: NavController,
+    public afAuth: AngularFireAuth,
+    private alertCtrl: AlertController
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  validateLogin() {
-    // some magic
-    console.log('SUCCESSFUL LOGIN\nNavigating to Voting...');
-    this.goToVoting();
+  async login() {
+    const { email, password } = this;
+
+    try {
+      const res = await this.afAuth.auth.signInWithEmailAndPassword(
+        email,
+        password
+      );
+      console.log(res);
+      this.goToVoting();
+    } catch (err) {
+      console.dir(err);
+      this.showAlert('Error', err.message);
+    }
   }
 
   goToVoting() {
     this.navCtrl.navigateRoot(`/tabs`);
   }
 
+  async showAlert(header: string, message: string) {
+    const alert = await this.alertCtrl.create({
+      header,
+      message,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
 }
--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { NavController, AlertController } from '@ionic/angular';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-register',
@@ -7,11 +8,50 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./register.page.scss']
 })
 export class RegisterPage implements OnInit {
-  constructor(private navCtrl: NavController) {}
+  email = '';
+  password = '';
+  confirmPassword = '';
+
+  constructor(
+    private navCtrl: NavController,
+    public afAuth: AngularFireAuth,
+    private alertCtrl: AlertController
+  ) {}
 
   ngOnInit() {}
 
+  async register() {
+    const { email, password, confirmPassword } = this;
+
+    if (password !== confirmPassword) {
+      this.showAlert('Error', 'Passwords do not match');
+      return console.error('Passwords do not match!');
+    }
+
+    try {
+      const res = await this.afAuth.auth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+      console.log(res);
+      this.showAlert('Success', 'Welcome aboard!');
+    } catch (err) {
+      console.dir(err);
+      this.showAlert('Error', err.message);
+    }
+  }
+
   goToLogin() {
     this.navCtrl.navigateRoot(`/login`);
+  }
+
+  async showAlert(header: string, message: string) {
+    const alert = await this.alertCtrl.create({
+      header,
+      message,
+      buttons: ['OK']
+    });
+
+    await alert.present();
   }
 }
