a4a27b03d2bf62d0911e71d5ddc039dfc7904908
Justin Moore <JustinTMoore@comcast.net>
2019-09-08 18:41:44 -0400

    Allow Android Back Button to Exit App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,11 +11,12 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar) {
+    private statusBar: StatusBar,
+    private router: Router
+  ) {
     this.initializeApp();
   }
 
@@ -22,6 +24,18 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.platform.backButton.subscribe(async () => {
+        if (
+          (this.router.isActive(`/tabs/voting`, true) &&
+            this.router.url === `/tabs/voting`) ||
+          (this.router.isActive(`/login`, true) &&
+            this.router.url === `/login`) ||
+          (this.router.isActive(`/register`, true) &&
+            this.router.url === `/register`)
+        ) {
+          navigator[`app`].exitApp();
+        }
+      });
     });
   }
 }
