d5007b5a7ada4f22625a6fd5c44bd70a5a40e216
Justin Moore <47158110+JMooreo@users.noreply.github.com>
2022-01-29 14:36:44 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,7 @@
 
 export const environment = {
   production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyCrdksp7ENPzGSUyjbFP3lR6hvTOjfaAp4",
-    authDomain: "fashionbuddy-e21c3.firebaseapp.com",
-    databaseURL: "https://fashionbuddy-e21c3.firebaseio.com",
-    projectId: "fashionbuddy-e21c3",
-    storageBucket: "fashionbuddy-e21c3.appspot.com",
-    messagingSenderId: "669845790704",
-    appId: "1:669845790704:web:105383d75169286e",
-  }
+  firebaseConfig: '[REDATCED]'
 };
 
 /*
